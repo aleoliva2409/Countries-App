f719933086ad457d297841da3d64e3a9dfec552c
aleoliva2409 <alejandro.oliva2409@gmail.com>
2021-08-03 17:56:39 -0300

    backend ready to deploy

--- api/initDB.js
+++ api/initDB.js
@@ -3,20 +3,25 @@ const { Country } = require("./src/db")
 
 const initDB = async() => {
   try {
-    const resp = await axios.get(`https://restcountries.eu/rest/v2/all`);
-    const countries = resp.data;
+    const countries = await Country.findAll()
+    if(countries.length === 250) {
+      return
+    } else {
+      const resp = await axios.get(`https://restcountries.eu/rest/v2/all`);
+      const countries = resp.data;
 
-    for (let i = 0; i < countries.length; i++) {
-      await Country.create({
-        id: countries[i].alpha3Code,
-        name: countries[i].name,
-        image: countries[i].flag,
-        continent: countries[i].region,
-        capital: countries[i].capital,
-        subregion: countries[i].subregion,
-        area: `${countries[i].area} km2`,
-        population: countries[i].population,
-      });
+      for (let i = 0; i < countries.length; i++) {
+        await Country.create({
+          id: countries[i].alpha3Code,
+          name: countries[i].name,
+          image: countries[i].flag,
+          continent: countries[i].region,
+          capital: countries[i].capital,
+          subregion: countries[i].subregion,
+          area: `${countries[i].area} km2`,
+          population: countries[i].population,
+        });
+      }
     }
 
   } catch (error) {
--- api/src/app.js
+++ api/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 const morgan = require("morgan");
 const routes = require("./routes/index.js");
 
@@ -10,12 +11,13 @@ const server = express();
 
 server.name = "API";
 
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
